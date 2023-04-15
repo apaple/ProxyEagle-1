7356e8ec817afe5dfa99f537a47b0f0fe1dc0893
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2023-04-15 12:23:21 +0200

    Update proxyeagle.py

--- proxyeagle.py
+++ proxyeagle.py
@@ -1,16 +1,18 @@
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
 from concurrent.futures import Future
-import socket
-from sys import exit,argv
+import socks
+from sys import exit, argv
 import os.path
 from os import cpu_count
+import json
 
 # Author: Z3NTL3
 # Studios: Pix4Devs
 # Contact: www.pix4.dev
 
-cores = (cpu_count() * 2) -2
+cores = (cpu_count() * 2) - 2
+
 
 def Usage():
     print(f"""
@@ -20,9 +22,14 @@ def Usage():
 [Example]
 > python3 {__file__} 6 proxies.txt http
 > python3 {__file__} 6 proxies.txt https
-> python3 {__file__} 6 proxies.txt socks
+> python3 {__file__} 6 proxies.txt socks4
+> python3 {__file__} 6 proxies.txt socks5
 """)
+
+
 goods = ""
+
+
 def Logo():
     print("""
 \033[38;5;206m╔═╗╦═╗╔═╗═╗ ╦╦ ╦  ╔═╗╔═╗╔═╗╦  ╔═╗
@@ -31,6 +38,7 @@ def Logo():
  \033[38;5;206mMade by Pix4Devs - Pix4.dev
  \033[38;5;207mby Z3NTL3\033[0m
 """)
+
 
 try:
     int(argv[1])
@@ -45,10 +53,11 @@ except:
     Usage()
     exit(-1)
 
-def FileRead(file = argv[2]):
+
+def FileRead(file=argv[2]):
     rox = ""
     if os.path.exists(argv[2]):
-        with open(f"{argv[2]}","r")as file:
+        with open(f"{argv[2]}", "r")as file:
             content = file.read().strip(" ").split("\n")
         for q in content:
             rox += q+"\n"
@@ -56,62 +65,67 @@ def FileRead(file = argv[2]):
     else:
         return False
 
+
 def ProxyConnector(**info):
     global goods
-    sockInit = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        if info['protocol'] == "http":
-            hp = 80
-            header = f"""GET / HTTP/1.1\r\n"""
-            header += f"Host: google.com:{hp}\r\n\r\n"
-
-        elif info['protocol'] == "https":
-            hp = 443
-            header = f"""GET / HTTP/1.1\r\n"""
-            header += f"Host: google.com:{hp}\r\n\r\n"
-        else:
-            hp = 80
-            header = f"""GET / HTTP/1.1\r\n"""
-            header += f"Host: google.com:{hp}\r\n\r\n"
-
+        sockInit = socks.socksocket()
+        header = f"""GET /headers HTTP/1.1\r\n"""
+        header += f"Host: httpbin.org\r\nX-Tool: ProxyEagle\r\n\r\n"
         port = int(info['port'])
+
+        connPort = 80
+
+        if info['protocol'] == "http":
+            sockInit.set_proxy(socks.HTTP, f"{info['proxy']}", port)
+        elif info['protocol'] == "https":
+            sockInit.set_proxy(socks.HTTP, f"{info['proxy']}", port)
+            connPort = 443
+        elif info['protocol'] == "socks4":
+            sockInit.set_proxy(socks.SOCKS4, f"{info['proxy']}", port)
+        elif info['protocol'] == "socks5":
+            sockInit.set_proxy(socks.SOCKS5, f"{info['proxy']}", port)
+        else:
+            sockInit.set_proxy(socks.SOCKS4, f"{info['proxy']}", port)
         sockInit.settimeout(int(argv[1]))
-        sockInit.connect((f"{info['proxy']}",port))
+        sockInit.connect(("httpbin.org", connPort))
+
         sended = sockInit.send(header.encode("utf-8"))
-        response = sockInit.recv(1042).decode()
+        response = sockInit.recv(4096).decode()
 
-        succes_codes = [x for x in range(200,299+1)]
-        info_codes = [x for x in range(100,199+1)]
-        red_codes = [x for x in range(300,399+1)]
+        data = json.loads(response.split("\r\n\r\n")[1])
 
-        workingProxy = False
-
-        for sc in succes_codes:
-            if str(sc) in response:
-                workingProxy = True
-
-        for sc in info_codes:
-            if str(sc) in response:
-                workingProxy = True
-
-        for sc in red_codes:
-            if str(sc) in response:
-                workingProxy = True
-
-        if workingProxy:
-            #print(response)
+        if (data["headers"]):
             goods += f"{info['proxy']}:{info['port']}\n"
-            with open('goods.txt',"a+")as file:
-                file.write(f"{info['proxy']}:{info['port']}\n")
-            return f"\033[32mGood proxy: \033[33m{info['proxy']}:{info['port']}\033[0m"
+
+            level = 3
+
+            if ("X-Forwarded-For" not in data["headers"] and "Via" not in data["headers"] and "From" not in data["headers"]):
+                level = 1
+                with open(f'lvl{level}.txt', "a+")as file:
+                    file.write(f"{info['proxy']}:{info['port']}\n")
+                return f"\033[32mGood proxy: \033[33m{info['proxy']}:{info['port']} \033[0m[\033[1mElite Proxy\033[0m]"
+            elif ("X-Forwarded-For" in data["headers"]):
+                level = 3
+                with open(f'lvl{level}.txt', "a+")as file:
+                    file.write(f"{info['proxy']}:{info['port']}\n")
+                return f"\033[32mGood proxy: \033[33m{info['proxy']}:{info['port']} \033[0m[\033[1mTransparent Proxy\033[0m]"
+            else:
+                level = 2
+                with open(f'lvl{level}.txt', "a+")as file:
+                    file.write(f"{info['proxy']}:{info['port']}\n")
+                return f"\033[32mGood proxy: \033[33m{info['proxy']}:{info['port']} \033[0m[\033[1mAnonymous Proxy\033[0m]"
         else:
             return f"\033[31mBad proxy: \033[33m{info['proxy']}:{info['port']}\033[0m"
+
     except Exception as e:
         if e == KeyboardInterrupt:
             return "keyboard"
         return f"\033[31mBad proxy: \033[33m{info['proxy']}:{info['port']}\033[0m"
     finally:
         sockInit.close()
+
+
 def CheckFile(data):
     invalid = False
     for check in data:
@@ -122,15 +136,18 @@ def CheckFile(data):
         return False
     else:
         return True
+
+
 def Main():
     ct = FileRead()
 
     if type(ct) is list:
         pass
     elif type(ct) is bool:
-        print(f"\033[31mFile: \033[33m\'{argv[2]}\'\033[0m does not exist in the current directory\033[0m")
+        print(
+            f"\033[31mFile: \033[33m\'{argv[2]}\'\033[0m does not exist in the current directory\033[0m")
         exit(-1)
-    
+
     pool = ThreadPoolExecutor(max_workers=int(cores))
     hosts = []
     ports = []
@@ -138,7 +155,7 @@ def Main():
     destroy = False
     for proxy in proxyWhiteSpaceFix:
         try:
-            h,p = proxy.split(":")
+            h, p = proxy.split(":")
         except:
             destroy = True
             break
@@ -153,9 +170,10 @@ def Main():
         print("\033[31mYour proxy file contains not a correct format!\nFormats needs to be like proxy:port in each line\033[0m")
         return exit(-1)
     else:
-       pass
+        pass
 
-    ftrs = [pool.submit(ProxyConnector,proxy=Worker[0],port=Worker[1],protocol=argv[3]) for Worker in zip(hosts,ports)]
+    ftrs = [pool.submit(ProxyConnector, proxy=Worker[0], port=Worker[1],
+                        protocol=argv[3]) for Worker in zip(hosts, ports)]
     try:
         for f in as_completed(ftrs):
             if f.result() == "keyboard":
@@ -165,9 +183,8 @@ def Main():
     except:
         pass
     finally:
-        pool.shutdown(wait=False,cancel_futures=True)
+        pool.shutdown(wait=False, cancel_futures=True)
+
 
 if __name__ == "__main__":
     Main()
-
-    
